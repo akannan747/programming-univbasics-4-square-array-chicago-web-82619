006145ad93b00a4ae700ca9e5427f60da69cd371
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-22 21:13:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   result = []
   while counter < array.length do
-    result.push(array[counter] ** 2)
+    result.push(array[counter++] ** 2)
   end
   return result
 end
